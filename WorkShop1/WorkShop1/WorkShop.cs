34719ef30cf9d5999a5361378946277afbf720c4
ranjithabr07 <ranjitharanjubrhlk1998@gmail.com>
2022-10-31 12:12:59 +0530

    initial commit

---
+++ WorkShop1/WorkShop1/WorkShop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkShop1
+{
+    internal class PrimeNumber
+    {
+        public static void PrimeFactor()
+        {
+            int n = 0;
+            int temp = 0;
+
+            Console.WriteLine("Enter the number to check prime");
+            n = Convert.ToInt32(Console.ReadLine());
+            int m = n / 2;
+
+            for (int i = 2; i < m; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine("Entered number is NOT Prime");
+                    temp = 1;
+                    break;
+
+                }
+
+
+            }
+            if (temp == 0)
+            {
+                Console.WriteLine("Enter Number is Prime");
+
+            }
+
+        }
+    }
+}
